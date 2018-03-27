820563838fbba65d217f0ebd9eb5f83caf38e709
jlerm <jlerm@us.ibm.com>
2018-03-27 11:26:55 -0500

    update...

--- VMware/terraform/hcl/hcsdl1/hcdsl1.tf
+++ VMware/terraform/hcl/hcsdl1/hcdsl1.tf
@@ -12,7 +12,7 @@ variable "allow_unverified_ssl" {
 ##############################################################
 provider "vsphere" {
   allow_unverified_ssl = "${var.allow_unverified_ssl}"
-  version = "~> 1.3"
+  version = "~> 1.3" 
 }
 
 provider "camc" {
