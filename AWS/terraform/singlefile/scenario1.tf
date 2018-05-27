5bf5019e41266d22aca6344e72a6a09f03f1d9d5
jlerm <jlerm@us.ibm.com>
2018-05-27 13:26:24 -0500

    update

--- AWS/terraform/singlefile/scenario1.tf
+++ AWS/terraform/singlefile/scenario1.tf
@@ -64,6 +64,20 @@ resource "aws_key_pair" "orpheus_public_key" {
   public_key = "${var.public_ssh_key}"
 }
 
+
+##############################################################
+# Create temp public key for ssh connection
+##############################################################
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+}
+
+resource "aws_key_pair" "temp_public_key" {
+  key_name   = "${var.public_key_name}-temp"
+  public_key = "${tls_private_key.ssh.public_key_openssh}"
+}
+
+
 resource "aws_instance" "orpheus_ubuntu_micro" {
   instance_type = "m4.large"
   ami           = "${var.aws_image}"
@@ -72,4 +86,26 @@ resource "aws_instance" "orpheus_ubuntu_micro" {
   root_block_device = { "volume_type" = "gp2", "volume_size" = "100", "delete_on_termination" = true }
   ebs_block_device = { "device_name" = "/dev/sdf", "volume_type" = "gp2", "volume_size" = "4000", "delete_on_termination" = true, "encrypted" = true }
   ebs_block_device = { "device_name" = "/dev/sdg", "volume_type" = "gp2", "volume_size" = "4000", "delete_on_termination" = true, "encrypted" = true }
+  
+  connection {
+    user        = "ec2-user"
+    private_key = "${tls_private_key.ssh.private_key_pem}"
+    host        = "${self.public_ip}"
+  }
+  
+   provisioner "file" {
+    content = <<EOF
+#!/bin/bash
+LOGFILE="/var/log/addkey.log"
+user_public_key=$1
+if [ "$user_public_key" != "None" ] ; then
+    echo "---start adding user_public_key----" | tee -a $LOGFILE 2>&1
+    echo "$user_public_key" | tee -a $HOME/.ssh/authorized_keys          >> $LOGFILE 2>&1 || { echo "---Failed to add user_public_key---" | tee -a $LOGFILE; exit 1; }
+    echo "---finish adding user_public_key----" | tee -a $LOGFILE 2>&1
+fi
+EOF
+
+    destination = "/tmp/test.sh"
+}
+
 }
