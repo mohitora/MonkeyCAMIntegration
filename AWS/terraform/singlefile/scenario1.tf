c4d6c9be095b8848918bd2a4d90cb1c9ad88f601
jlerm <jlerm@us.ibm.com>
2018-05-27 12:50:03 -0500

    Added AWS template for one VM

---
+++ AWS/terraform/singlefile/scenario1.tf
@@ -0,0 +1,72 @@
+#################################################################
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Licensed Materials - Property of IBM
+#
+# ©Copyright IBM Corp. 2017, 2018.
+#
+#################################################################
+
+provider "aws" {
+  version = "~> 1.2"
+  region  = "${var.aws_region}"
+}
+
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default     = "us-east-1"
+}
+
+variable "vpc_name_tag" {
+  description = "Name of the Virtual Private Cloud (VPC) this resource is going to be deployed into"
+}
+
+variable "subnet_cidr" {
+  description = "Subnet cidr"
+}
+
+data "aws_vpc" "selected" {
+  state = "available"
+
+  filter {
+    name   = "tag:Name"
+    values = ["${var.vpc_name_tag}"]
+  }
+}
+
+data "aws_subnet" "selected" {
+  state      = "available"
+  vpc_id     = "${data.aws_vpc.selected.id}"
+  cidr_block = "${var.subnet_cidr}"
+}
+
+variable "public_ssh_key_name" {
+  description = "Name of the public SSH key used to connect to the virtual guest"
+}
+
+variable "public_ssh_key" {
+  description = "Public SSH key used to connect to the virtual guest"
+}
+
+#Variable : AWS image name
+variable "aws_image" {
+  type = "string"
+  description = "Operating system image id / template that should be used when creating the virtual image"
+  default = "ami-c998b6b2"
+}
+
+resource "aws_key_pair" "orpheus_public_key" {
+  key_name   = "${var.public_ssh_key_name}"
+  public_key = "${var.public_ssh_key}"
+}
+
+resource "aws_instance" "orpheus_ubuntu_micro" {
+  instance_type = "t4.large"
+  ami           = "${var.aws_image}"
+  subnet_id     = "${data.aws_subnet.selected.id}"
+  key_name      = "${aws_key_pair.orpheus_public_key.id}"
+}
