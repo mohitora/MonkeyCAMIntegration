56143f53791ded271327f6fce6172077973b0af6
jlerm <jlerm@us.ibm.com>
2018-05-27 17:20:10 -0500

    update

--- AWS/terraform/HCSDL-AWS/hcsdl-aws.tf
+++ AWS/terraform/HCSDL-AWS/hcsdl-aws.tf
@@ -174,17 +174,13 @@ resource "aws_key_pair" "temp_public_key" {
 # Driver
 #
 resource "aws_instance" "driver" {
-  count         = "2"
-#  hostname      = "${var.vm_name_prefix}-driver-${ count.index }"
-#  domain        = "${var.vm_domain}"
-  tags { Name = "driver-${ count.index }.${var.vm_domain}" }
+  count         = "1"
+  tags { Name = "driver.${var.vm_domain}" }
   instance_type = "m4.large"
   ami           = "${var.aws_image}"
   subnet_id     = "${data.aws_subnet.selected.id}"
   key_name      = "${aws_key_pair.temp_public_key.id}"
   root_block_device = { "volume_type" = "gp2", "volume_size" = "100", "delete_on_termination" = true }
-  ebs_block_device = { "device_name" = "/dev/sdf", "volume_type" = "gp2", "volume_size" = "4000", "delete_on_termination" = true, "encrypted" = true }
-  ebs_block_device = { "device_name" = "/dev/sdg", "volume_type" = "gp2", "volume_size" = "4000", "delete_on_termination" = true, "encrypted" = true }
   
   connection {
     user        = "ec2-user"
@@ -209,9 +205,9 @@ EOF
 
   provisioner "remote-exec" {
     inline = [
-      "echo \"driver-${ count.index }.${var.vm_domain}\">/tmp/hostname",
+      "echo \"driver.${var.vm_domain}\">/tmp/hostname",
       "sudo mv /tmp/hostname /etc/hostname",
-      "sudo hostname \"driver-${ count.index }.${var.vm_domain}\"",
+      "sudo hostname \"driver.${var.vm_domain}\"",
       "sudo chmod +x /tmp/addkey.sh; sudo bash /tmp/addkey.sh \"${var.public_ssh_key}\"",
       "sudo sed -i -e 's/# %wheel/%wheel/' -e 's/Defaults    requiretty/#Defaults    requiretty/' /etc/sudoers",
       "sudo useradd ${var.sudo_user}",
@@ -257,5 +253,53 @@ EOF
     destination = "/tmp/installation.sh"
 
   }
+  
+}
+
+
+
+resource "aws_instance" "idm" {
+  count         = "2"
+  tags { Name = "idm-${ count.index }.${var.vm_domain}" }
+  instance_type = "m4.large"
+  ami           = "${var.aws_image}"
+  subnet_id     = "${data.aws_subnet.selected.id}"
+  key_name      = "${aws_key_pair.temp_public_key.id}"
+  root_block_device = { "volume_type" = "gp2", "volume_size" = "100", "delete_on_termination" = true }
+  
+  connection {
+    user        = "ec2-user"
+    private_key = "${tls_private_key.ssh.private_key_pem}"
+    host        = "${self.public_ip}"
+  }
+  
+   provisioner "file" {
+    content = <<EOF
+#!/bin/bash
+LOGFILE="/var/log/addkey.log"
+user_public_key=$1
+if [ "$user_public_key" != "None" ] ; then
+    echo "---start adding user_public_key----" | tee -a $LOGFILE 2>&1
+    echo "$user_public_key" | tee -a $HOME/.ssh/authorized_keys          >> $LOGFILE 2>&1 || { echo "---Failed to add user_public_key---" | tee -a $LOGFILE; exit 1; }
+    echo "---finish adding user_public_key----" | tee -a $LOGFILE 2>&1
+fi
+EOF
+
+    destination = "/tmp/addkey.sh"
+}
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo \"idm-${ count.index }.${var.vm_domain}\">/tmp/hostname",
+      "sudo mv /tmp/hostname /etc/hostname",
+      "sudo hostname \"idm-${ count.index }.${var.vm_domain}\"",
+      "sudo chmod +x /tmp/addkey.sh; sudo bash /tmp/addkey.sh \"${var.public_ssh_key}\"",
+      "sudo sed -i -e 's/# %wheel/%wheel/' -e 's/Defaults    requiretty/#Defaults    requiretty/' /etc/sudoers",
+      "sudo useradd ${var.sudo_user}",
+      "sudo echo ${var.sudo_password} | passwd ${var.sudo_user} --stdin",
+      "sudo usermod ${var.sudo_user} -g wheel"
+    ]
+ }
+
   
 }
